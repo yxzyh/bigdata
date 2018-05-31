3f497ec5fd5823645afbb654cfa0fc9b9a9d8ed3
yxzyh <892873967@qq.com>
2018-05-31 10:49:57 +0800

    added by Geoge 20180531

---
+++ dn2fis01/src/com/fis/test/Test.java
@@ -0,0 +1,12 @@
+package com.fis.test;
+
+/**
+ * @author Geoge
+ * @date 2018/5/8 0008 上午 10:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        
+    }
+}
